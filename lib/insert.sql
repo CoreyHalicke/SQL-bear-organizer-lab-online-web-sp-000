6ffd62436a6905f046a33a4637cf7b74eb727a43
Corey H <c.l.halicke@gmail.com>
2019-08-21 01:38:57 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,22 +1,22 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive)
   VALUES ("Mr. Chocolate", 3, "Male", "Brown", "Mild", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ("Rowdy", 5, "Female", "Black", "Aggressive", 0);
+  VALUES ("Rowdy", 5, "Male", "Black", "Aggressive", 0);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ("Tabitha", 4, "Male", "White", "Medium", 1);
+  VALUES ("Tabitha", 4, "Female", "White", "Medium", 1);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ("Sergeant Brown", 5, "Female", "Black", "Medium", 1);
+  VALUES ("Sergeant Brown", 5, "Male", "Black", "Medium", 1);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ("Melissa", 7, "Male", "White", "Mild", 0);
+  VALUES ("Melissa", 7, "Female", "White", "Mild", 0);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ("Grinch", 9, "Female", "Black", "Aggressive", 0);
+  VALUES ("Grinch", 9, "Male", "Black", "Aggressive", 0);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-  VALUES ("Wendy", 2, "Male", "White", "Medium", 1);
+  VALUES ("Wendy", 2, "Female", "White", "Medium", 1);
 
 INSERT INTO bears (age, gender, color, temperament, alive)
-  VALUES (8, "Female", "White", "Aggressive", 0);
+  VALUES (8, "Male", "White", "Aggressive", 0);
